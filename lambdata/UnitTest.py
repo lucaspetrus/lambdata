d8a16bae6d2008b1f5d0ecb29284528ff9435c83
lucaspetrus <lpetrus16@gmail.com>
2020-09-03 16:01:42 -0500

    Made a unit test for OverwatchCharacters.py

--- lambdata/UnitTest.py
+++ lambdata/UnitTest.py
@@ -1,16 +1,26 @@
+# UnitTests for OverwatchCharacters.py in Lambdata
+
+
 import unittest
-from lambdata import Overwatch
-
-
+from lambdata import OverwatchCharacters
 
 
 class OverwatchTests(unittest.TestCase):
     """Tests the attributes of Overwatch Characters"""
     def test_health(self):
-        person = Overwatch()
+        """Test to make sure health is 5"""
+        person = OverwatchCharacters.Overwatch()
         self.assertEqual(person.health, 5)
 
+    def test_speed(self):
+        """Tests to make sure speed is 6"""
+        player = OverwatchCharacters.Overwatch()
+        self.assertEqual(player.speed, 6)
 
+    def test_damage(self):
+        """Tests to make sure speed is 0"""
+        player = OverwatchCharacters.Overwatch()
+        self.assertEqual(player.damage, 0)
 
 
 if __name__ == '__main__':
